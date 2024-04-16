9559978c0ddced3a85b8e2dd31897f7b077340df
Alessandro Marina <71550720+Hackingale@users.noreply.github.com>
2024-04-16 15:14:49 +0200

    LLM working offline
    gpt4all loca

--- LLM1.py
+++ LLM1.py
@@ -1,12 +1,8 @@
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+from gpt4all import GPT4All
+model = GPT4All("Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf", n_ctx = 10000, n_threads = 8, allow_download = True) # device='amd', device='intel'
 
-model_name = "deepset/roberta-base-squad2"
-
-# a) Get predictions
-nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
-QA_input = {
-    'question': 'Who is Cecilia?',
-    'context': 'Cecilia is the smallest patata in the world'
-}
-res = nlp(QA_input)
-print(res)
+with model.chat_session():
+    while True:
+        prompt = input("User: ")
+        output = model.generate(prompt, max_tokens=4096)
+        print(output)
--- scraper.py
+++ scraper.py
@@ -1,17 +1,25 @@
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-url = 'https://www.linkedin.com/company/'
+url = 'https://www.'
 # Read the Excel file
 df = pd.read_excel('Manual Classification.xlsx')
 
-company = df['Company'].apply(lambda x: x.split('.')[0])
+company = df['Company']
 
-for index in range(len(company)):
+for index in range(1):
     first_value = company.iloc[index]
     response = requests.get(url + first_value)
     if response.status_code == 200:
-        print(f"{first_value} is reachable")
+        # Parse the HTML content of the web page
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Extract only the text from the parsed HTML
+        text = ' '.join(line.strip() for line in soup.get_text().splitlines() if line.strip())
+
+        # Print the extracted text
+        print(text)
     else:
         print(response.status_code)
 
