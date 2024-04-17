a3869a026c8d6d592b19d80cb33a900c63665281
Marco Laurenzi <marco.laurenzi@mail.polimi.it>
2024-04-17 12:01:40 +0200

    Modified something to be sure it works on my machine

--- LLM1.py
+++ LLM1.py
@@ -1,6 +1,13 @@
 from gpt4all import GPT4All
 import functions as func
-model = GPT4All("/Users/alessandrom/Library/Application Support/nomic.ai/GPT4All/Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf", n_ctx = 10000, n_threads = 8, allow_download = True)
+import getpass
+
+#get current user function
+def get_current_user():
+    return getpass.getuser()
+
+path_to_gpt = "Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf" # works on my machine, let's see if it works on yours
+model = GPT4All(path_to_gpt, n_ctx = 10000, n_threads = 8, allow_download = True)
 
 def contextest():
     with model.chat_session():
--- main.py
+++ main.py
@@ -2,7 +2,3 @@ import google.generativeai as ai
 import functions as f
 import config
 import LLM1 as ai
-
-
-
-
