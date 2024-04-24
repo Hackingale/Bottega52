5078c160b490949b34d3a3d8425ebd538dc8a8a4
Marco Laurenzi <marco.laurenzi@mail.polimi.it>
2024-04-24 14:59:30 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,25 +1,32 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-url = 'https://www.'
-# Read the Excel file
-df = pd.read_excel('Manual Classification.xlsx')
 
-company = df['Company']
+# Function to scrape the web page and extract the text content from the HTML
+# todo - deal with sites not reachable from the simple comapnyName.com 
+def web_scraper(file):
+    # Read the Excel file
+    df = pd.read_excel(file, header=None, usecols=[3], skiprows=[0], names=['Company'])
 
-for index in range(1):
-    first_value = company.iloc[index]
-    response = requests.get(url + first_value)
-    if response.status_code == 200:
-        # Parse the HTML content of the web page
-        soup = BeautifulSoup(response.content, 'html.parser')
+    company_column = df['Company']
 
-        # Extract only the text from the parsed HTML
-        text = ' '.join(line.strip() for line in soup.get_text().splitlines() if line.strip())
+    for company_name in company_column:
+        if pd.notnull(company_name):  # Check if company name is not null
+            url = 'https://www.' + company_name + '.com'  # Replace example.com with your base URL
+            response = requests.get(url)
 
-        # Print the extracted text
-        print(text)
-    else:
-        print(response.status_code)
+            if response.status_code == 200:
+                # Parse the HTML content of the web page
+                soup = BeautifulSoup(response.content, 'html.parser')
 
+                # Extract only the text from the parsed HTML
+                text = ' '.join(line.strip() for line in soup.get_text().splitlines() if line.strip())
+
+                # Print the extracted text
+                print(text)
+            else:
+                print("Failed to fetch:", url, "Status Code:", response.status_code)
+        else:
+            print("Company name is null or empty.")
+
+web_scraper("InputData.xlsx")
