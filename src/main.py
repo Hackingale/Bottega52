7e05f58ab535565ea4da1c1713a27be0f59db548
Alessandro Marina <71550720+Hackingale@users.noreply.github.com>
2024-05-01 21:59:55 +0200

    Update main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from LLM1 import ConversationHandler, get_external_input
-
+import time
 '''if __name__ == '__main__':
     fileupload.app.run(debug=True)
 '''
@@ -14,13 +14,16 @@ f.file_sguccer(f.create_buyers('/Users/marco/Developer/GitHub/Botteg52/xlsx file
 model_path = "/Users/alessandrom/Library/Application Support/nomic.ai/GPT4All/Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf"
 
 handler = ConversationHandler(model_path)
-
 handler.start()
 
-
-# Simulate external input being received
-prompt = input("Enter a prompt: ")
-user_input = get_external_input(prompt)
-handler.send_input(user_input)
-
-
+time.sleep(2)
+while True:
+    time.sleep(0.5)
+    handler.lock.acquire()
+    prompt = input("Enter a prompt: ")
+    user_input = get_external_input(prompt)
+    if(user_input == "exit"):
+        handler.stop()
+        break
+    handler.send_input(user_input)
+    handler.lock.release()
