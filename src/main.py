41cc18f9feca80f843e4463a48a7760095cde49d
Alessandro Marina <71550720+Hackingale@users.noreply.github.com>
2024-05-02 15:00:28 +0200

    Main updated

--- src/main.py
+++ src/main.py
@@ -11,8 +11,9 @@ df = f.countemployees('/Users/marco/Developer/GitHub/Botteg52/xlsx files/InputDa
 f.file_sguccer(f.create_buyers('/Users/marco/Developer/GitHub/Botteg52/xlsx files/Categories.xlsx'), f.create_targets('/Users/marco/Developer/GitHub/Botteg52/xlsx files/Categories.xlsx'), f.create_influencers('/Users/marco/Developer/GitHub/Botteg52/xlsx files/Categories.xlsx'), df)
 
 '''
-model_path = "/Users/alessandrom/Library/Application Support/nomic.ai/GPT4All/Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf"
+#model_path = "/Users/alessandrom/Library/Application Support/nomic.ai/GPT4All/Nous-Hermes-2-Mistral-7B-DPO.Q4_0.gguf"
 
+model_path = '/Users/alessandrom/Library/Application Support/nomic.ai/GPT4All/Meta-Llama-3-8B-Instruct.Q4_0.gguf'
 handler = ConversationHandler(model_path)
 handler.start()
 
@@ -27,3 +28,6 @@ while True:
         break
     handler.send_input(user_input)
     handler.lock.release()
+
+
+#it is correct! now just provide me an answer containing only one word: the category you found without emojis or other characters add this to generate a precise output
